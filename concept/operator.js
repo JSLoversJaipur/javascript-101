436597a800a61a698561697a18720b78279bcc41
Janit Yadav <janityadav16@gmail.com>
2023-08-08 21:28:26 +0530

    to make learning of operators in javascript easier

---
+++ concept/operator.js
@@ -0,0 +1,55 @@
+// Arithmetic Operators
+let x = 5;
+let y = 2;
+let sum = x + y; // Addition
+let difference = x - y; // Subtraction
+let product = x * y; // Multiplication
+let quotient = x / y; // Division
+let remainder = x % y; // Modulo (remainder of division)
+
+// Assignment Operators
+let a = 10;
+let b = 5;
+a += b; // Equivalent to a = a + b; (Addition assignment)
+a -= b; // Equivalent to a = a - b; (Subtraction assignment)
+a *= b; // Equivalent to a = a * b; (Multiplication assignment)
+a /= b; // Equivalent to a = a / b; (Division assignment)
+a %= b; // Equivalent to a = a % b; (Modulo assignment)
+
+// Comparison Operators
+let num1 = 10;
+let num2 = 5;
+let isEqual = num1 == num2; // Equality check
+let strictEquality = num1 === num2; // Strict equality check
+let isNotEqual = num1 != num2; // Inequality check
+let isGreater = num1 > num2; // Greater than check
+let isLess = num1 < num2; // Less than check
+let isGreaterOrEqual = num1 >= num2; // Greater than or equal to check
+let isLessOrEqual = num1 <= num2; // Less than or equal to check
+
+// Logical Operators
+let isTrue = true;
+let isFalse = false;
+let logicalAnd = isTrue && isFalse; // Logical AND
+let logicalOr = isTrue || isFalse; // Logical OR
+let logicalNot = !isTrue; // Logical NOT
+
+// Increment and Decrement Operators
+let counter = 0;
+counter++; // Increment by 1
+counter--; // Decrement by 1
+
+// Concatenation Operator (for strings)
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName; // Concatenation
+
+// Ternary Operator
+let isAdult = true;
+let status = isAdult ? "Adult" : "Minor"; // Ternary operator for conditional assignment
+
+// typeof Operator
+let number = 42;
+let type = typeof number; // "typeof" operator to get the data type of a value
+
+
